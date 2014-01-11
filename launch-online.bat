201005bde0023b66b72b7d50000ba8ae67fda978
Denny Kunker <wkunker@gmail.com>
2014-01-11 04:58:54 -0500

    launch-online.bat now tests internet connection, and only launches autoav upon successful connecting. This will later serve to filter out network related issues--providing fallbacks for situations where, for example, network components are hijacked, and preliminary scans need to be run to continue (amongst many other possibilities).

--- launch-online.bat
+++ launch-online.bat
@@ -1,3 +1,15 @@
+@echo off
+echo Testing internet connection...
 cd /D %~dp0
+Ping www.google.com -n 1 -w 1000
+if errorlevel 1 goto failure
+if errorlevel 0 goto success
+:failure
+echo Not Connected! Eventually this will hook into a network repair component. Exiting...
+pause
+exit
+:success
+echo Success!
 call downloadall.bat
 call launch-offline.bat
+@echo on
